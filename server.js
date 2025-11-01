194c8193fdc307a03d3e7bc1d463fc78dbd48a61
Chris <106016563+Xtosea@users.noreply.github.com>
2025-11-01 13:08:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "https://viralvideos.vercel.app",
   "https://viralvideoplus.vercel.app",
+  "https://www.trendwatch.i.ng/login",
 ];
 
 // ✅ Dynamic CORS Middleware
