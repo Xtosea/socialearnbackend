1b4f18db00fdba720ff325c99538eed617556291
Chris <106016563+Xtosea@users.noreply.github.com>
2025-12-29 14:48:01 +0100

    Update pointsHelpers.js

--- utils/pointsHelpers.js
+++ utils/pointsHelpers.js
@@ -1,18 +1,30 @@
 // utils/pointsHelpers.js
 import History from "../models/HistoryLog.js";
 
+/**
+ * Update user points and log history, then emit socket update
+ * @param {Object} params
+ * @param {Object} params.user - Mongoose User document
+ * @param {number} params.amount - Points to add/subtract
+ * @param {string} params.taskType - e.g., "video-view", "daily-login"
+ * @param {ObjectId} [params.taskId] - Optional Task reference
+ * @param {string} [params.description] - Description
+ * @param {Object} [params.metadata] - Extra metadata
+ * @param {Object} [params.req] - Express req object to access io
+ */
 export const updateUserPoints = async ({
   user,
   amount,
   taskType,
-  taskId,
-  description,
+  taskId = null,
+  description = "",
   metadata = {},
+  req = null,
 }) => {
   user.points += amount;
   await user.save();
 
-  await History.create({
+  const historyEntry = await History.create({
     user: user._id,
     taskType,
     taskId,
@@ -20,6 +32,14 @@ export const updateUserPoints = async ({
     description,
     metadata,
   });
+
+  // Emit socket update if req.app.get('io') is available
+  if (req?.app?.get("io")) {
+    req.app.get("io").to(user._id.toString()).emit("pointsUpdate", {
+      points: user.points,
+      newEntryId: historyEntry._id,
+    });
+  }
 
   return user;
 };
