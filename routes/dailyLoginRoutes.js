b829dd0f3e1c774b4dd65310ca06ce0cd06e2b03
Chris <106016563+Xtosea@users.noreply.github.com>
2025-12-29 01:05:26 +0100

    Create dailyLoginRoutes.js

---
+++ routes/dailyLoginRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { dailyLoginReward } from "../controllers/dailyLoginController.js";
+import protect from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/daily-login", protect, dailyLoginReward);
+
+export default router;
