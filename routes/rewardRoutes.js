d4f433a7bf66fd3db3d231a8d76d7ef06c65db65
Chris <106016563+Xtosea@users.noreply.github.com>
2025-12-29 21:23:14 +0100

    Update rewardRoutes.js

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -1,10 +1,9 @@
-// routes/rewardRoutes.js
 import express from "express";
 import { dailyLoginReward } from "../controllers/dailyLoginController.js";
-import protect from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/daily-login", protect, dailyLoginReward);
+router.post("/daily-login", auth, dailyLoginReward);
 
 export default router;
