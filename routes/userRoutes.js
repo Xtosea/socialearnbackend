89b9eebad6e5c5bb1265c31e8dfd82468253d1ec
Chris <106016563+Xtosea@users.noreply.github.com>
2025-12-31 01:44:08 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,23 +12,7 @@ import protect from "../middleware/auth.js"; // JWT auth
 
 const router = express.Router();
 
-// ================= PROFILE =================
-router.get("/me", protect, getProfile);
-router.put("/me", protect, updateProfile);
 
-// ================= SUGGESTED USERS =================
-router.get("/suggested", protect, async (req, res) => {
-  try {
-    const currentUserId = req.user._id;
-    const users = await User.find({ _id: { $ne: currentUserId } })
-      .select("-password") // hide passwords
-      .limit(10); // adjust as needed
-    res.json(users);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
 
 // ================= SOCIAL (FOLLOW/UNFOLLOW) =================
 router.put("/follow/:id", protect, followUser);
