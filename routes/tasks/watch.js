935357d54df0a4704f466c2f9b2578b039323c7f
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-18 21:04:28 +0100

    Update watch.js

--- routes/tasks/watch.js
+++ routes/tasks/watch.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Task from "../../models/Task.js";
-import User from "../models/User.js";
-import auth from "../middleware/auth.js";
+import User from "../../models/User.js";
+import auth from "../../middleware/auth.js";
 
 const router = express.Router();
 
